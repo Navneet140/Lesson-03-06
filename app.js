b83b6a36c8e20f4f4c119f7db8ce245574ffe214
Navneet Singh <200413180@student.georgianc.on.ca>
2020-06-12 15:20:00 -0400

    uploading

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
+app.use('/css', express.static('assets/css'));
+app.use('/javascript', express.static('assets/javascript'));
+app.use('/images', express.static('assets/images'));
+
+// Mongo access
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URI, {
+  auth: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).catch(err => console.error(`Error: ${err}`));
+
+// Implement Body Parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 //defining the routes
 const routes = require('./routes.js');
 app.use('/', routes);
--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
-
+//our resource routes
 (require('./routes/pages'))(router);
+(require('./routes/blogs'))(router);
 
 module.exports = router;
