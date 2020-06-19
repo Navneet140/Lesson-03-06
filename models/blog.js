f0e96ce6e316ed3659098cad43d548407b290678
Navneet Singh <200413180@student.georgianc.on.ca>
2020-06-18 21:01:42 -0400

    finosed

--- models/blog.js
+++ models/blog.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const BlogSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true 
+    required: true // This must exist
   },
   content: {
     type: String,
@@ -30,5 +30,13 @@ BlogSchema.query.published = function () {
     status: 'PUBLISHED'
   })
 };
+
+BlogSchema.virtual('synopsis')
+.get(function () {
+  const post = this.content;
+  return post
+    .replace(/(<([^>]+)>)/ig,"")
+    .substring(0, 250);
+});
 
 module.exports = mongoose.model('Blog', BlogSchema);
