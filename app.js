3039dff179c45f8d7517423947de4a01403b870a
Navneet Singh <200413180@student.georgianc.on.ca>
2020-07-23 05:24:59 -0400

    done

--- app.js
+++ app.js
@@ -67,8 +67,18 @@ app.use('/', (req, res, next) => {
 
 // Our routes
 const routes = require('./routes.js');
-app.use('/', routes);
+app.use('/api', routes);
+
+app.get('/test', (req, res) => {
+  res.status(200).json({message: 'Hello World'});
+});
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 
 // Start our server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
